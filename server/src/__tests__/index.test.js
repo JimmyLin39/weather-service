8a068b04da4d29d660575a476059ff92ade10434
JimmyLin39 <jimmylin0309@email.com>
2020-03-21 13:08:49 -0700

    set up tests

---
+++ server/src/__tests__/index.test.js
@@ -0,0 +1,27 @@
+const request = require('supertest')
+const generateApp = require('../application')
+
+describe('Weather', () => {
+  let app
+  beforeAll(() => {
+    app = generateApp()
+  })
+
+  test('GET /v1/weather no query city', async () => {
+    await request(app)
+      .get('/v1/weather')
+      .expect(422)
+  })
+
+  test('GET /v1/weather?city=Vanvouer', async () => {
+    await request(app)
+      .get('/v1/weather?city=Vancouver')
+      .expect(200)
+  })
+
+  test('GET /v1/weather error city not found', async () => {
+    await request(app)
+      .get('/v1/weather?city=ca')
+      .expect(404, { cod: '404', message: 'city not found' })
+  })
+})
