f962e60dc47f419af6d48b5899ead479f1b362ca
JimmyLin39 <jimmylin0309@email.com>
2020-03-21 11:12:11 -0700

    get current weather from OpenWeather

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,19 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
+require('./environment')
+const axios = require('axios')
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/v1/weather', (req, res) => {
+  axios
+    .get(
+      `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${process.env.OPEN_WEATHER_KEY}`
+    )
+    .then(weatherRes => {
+      return res.send(weatherRes.data)
+    })
+    .catch(error => console.error(error))
+})
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}.`)
